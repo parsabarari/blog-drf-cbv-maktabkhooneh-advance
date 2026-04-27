6e589cd08cd5c5ad7129b0ff668df5ac764c07f2
Parsa <parsaperix@gmail.com>
2026-04-27 18:54:53 +0330

    added registration using api to project
    -added api(rest framework) to project
    -created urls for accounts app
    -created serializer and apiview for registration

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from . import views
+
+app_name = 'api-v1'
+
+urlpatterns = [
+    # registration
+    path('registration/',views.RegistrationApiView.as_view(),name = 'registration')
+    # change password
+    # reset password
+    # login token
+    # login jwt
+
+]
--- core/core/urls.py
+++ core/core/urls.py
@@ -39,6 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
+    path('accounts/',include('accounts.urls')),
     path('api-auth',include('rest_framework.urls')),
     path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
